6bddbf7681452228cb4c9a9242ce459f560c32c0
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-24 18:26:59 +0800

    remove unused

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,21 +60,11 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
         'gradient': {
           to: { 'background-position': '200% center' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
         "gradient": 'gradient 8s linear infinite',
       },
     },
